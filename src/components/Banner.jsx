ca5c2bcbbc570f04d2a9222f503dd8402314c79c
Jollypru <Jollyprumarma2020@gmail.com>
2024-12-15 20:12:18 +0600

    changed banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-                    className="w-full" />
+                    src="https://i.ibb.co.com/4VSVY97/6618.jpg"
+                    className="w-full h-[400px]" />
                 <div className='absolute inset-0 bg-black bg-opacity-50 text-white text-center flex flex-col items-center justify-center px-10'>
                     <h2 className='text-3xl md:text-5xl lg:text-6xl font-bold mb-4'>Simplify Your Visa Journey</h2>
                     <p className='text-xl md:text-2xl'>From requirements to application and tracking—everything in one place.</p>
@@ -18,8 +18,8 @@ const Banner = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-                    className="w-full" />
+                    src="https://i.ibb.co.com/VVBYXdw/3425297.jpg"
+                    className="w-full h-[400px]" />
                  <div className='absolute inset-0 bg-black bg-opacity-50 text-white text-center flex flex-col items-center justify-center'>
                     <h2 className='text-3xl md:text-5xl lg:text-6xl font-bold mb-4'>Your Global Gateway</h2>
                     <p className='text-2xl'>Explore visa requirements for any country with ease.</p>
@@ -31,8 +31,8 @@ const Banner = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-                    className="w-full" />
+                    src="https://i.ibb.co.com/Jq4jZzT/customs-officer-device-4.jpg"
+                    className="w-full h-[400px]" />
                  <div className='absolute inset-0 bg-black bg-opacity-50 text-white text-center flex flex-col items-center justify-center'>
                     <h2 className='text-3xl md:text-5xl lg:text-6xl font-bold mb-4'>Fast & Reliable Visa Processing</h2>
                     <p className='text-xl md:text-2xl'>Apply online and stay updated on your application's status.</p>
@@ -44,8 +44,8 @@ const Banner = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-                    className="w-full" />
+                    src="https://i.ibb.co.com/Bwsr1YQ/4689588.jpg"
+                    className="w-full h-[400px]" />
                  <div className='absolute inset-0 bg-black bg-opacity-50 text-white text-center flex flex-col items-center justify-center'>
                     <h2 className='text-3xl md:text-5xl lg:text-6xl font-bold mb-4'>Navigate the World With Confidence</h2>
                     <p className='text-xl md:text-2xl'>A seamless visa experience tailored for your convenience.</p>
